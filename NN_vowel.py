a93ff5599b0c7fc2cc9e6d833c9d9801e441c6d9
Jake <Jakepajouhesh@yahoo.com>
2026-02-11 15:59:36 -0800

    Implemented: Backpropogation(dScores, Gradient, Backpropogation to hidden layer, zero out <= 0, Update weights & biases

--- NN_vowel.py
+++ NN_vowel.py
@@ -62,28 +62,82 @@ class VowelNN:
     def process_data(self):
         pass
 
-    def train_model(self, epochs: int):
+
+    def train_model(self, epochs = 100):
         self.loss_history = []
         self.accuracy_history = []
         
-        for epoch in range(epochs):
+        x = self.x_train
+        y = self.y_train
+        for epoch in range(epochs): #run through the data_set 100 times 
 
-            #forward pass
+            #Forward Pass
+            #Input --> Hidden Layer 1
+            weight_sum_1 = np.dot(x, self.W1) + self.B1
+            output_H1_layer = np.maximum(0, weight_sum_1)
 
-            #compute loss
+            #Hidden --> Output
+            scores = np.dot(output_H1_layer, self.W2) + self.B2
+
+            probabilities = self.softmax(scores)
+            
+            
+            
+            #Compute Loss
+            N = y.shape[0]
+            prevent_crash = 1e-12
+            correct_probs = probabilities[np.arange(N), y]
+            loss = -np.mean(np.log(correct_probs + prevent_crash))
 
             #compute accuracy
+            predictions = np.argmax(probabilities, axis = 1) #pick highest prob vowel per row
+            accuracy = np.mean(predictions == y) #T/F accuracy per row
 
             #backpropagation
+            
 
             #update weights and biases
 
+            self.loss_history.append(loss)
+            self.accuracy_history.append(accuracy)
 
 
         #after training, plot the loss and accuracy history
-        plt.plot(self.loss_history)
-        plt.plot(self.accuracy_history)
+        plt.plot(self.loss_history, label = "Loss")
+        plt.plot(self.accuracy_history, label = "Accuracy")
+        plt.legend()
         plt.show()
+
+    def back_propagation(self, x, y, probabilities, weight_sum_1, output_H1_layer):
+        N = x.shape[0]
+
+        #dScores = dL/dScores 
+        dScores = probabilities.copy()
+        dScores[np.arange(N), y] -= 1
+        dScores /= N
+
+        #Gradient for W2 & B2
+        dW2 = np.dot(output_H1_layer.T, dScores)
+        dB2 = np.sum(dScores, axis = 0)
+
+        #Backprop into hidden layer
+        dHidden = np.dot(dScores, self.W2.T)
+
+        #zero out where pre-activation was <= 0
+        dHidden[weight_sum_1 <= 0] = 0
+
+        #Gradient for W1 & B1
+        dW1 = np.dot(x.T, dHidden)
+        dB1 = np.sum(dHidden, axis = 0)
+
+        #Update weights and biases
+        self.W1 -= self.LR * dW1
+        self.B1 -= self.LR * dB1
+        self.W2 -= self.LR * dW2
+        self.B2 -= self.LR * dB2
+
+        return dW1, dB1, dW2, dB2
+
 
 
     def softmax(self, z):
